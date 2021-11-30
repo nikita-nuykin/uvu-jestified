313989815c5c87cb53562476205b555eb0943f96
Nikita Nuykin <nuykinn@gmail.com>
2021-11-30 21:40:08 +0500

    Add test for describe function

---
+++ src/describe/describe.test.ts
@@ -0,0 +1,17 @@
+import { test } from 'uvu';
+import { equal } from 'uvu/assert';
+import { describe } from './describe';
+
+let value = 1;
+
+describe('Describe works as expected', it => {
+  it('Test', () => {
+    equal(++value, 2);
+  });
+});
+
+test('Checks if test inside describe was executed', () => {
+  equal(value, 2);
+});
+
+test.run();
--- src/describe/describe.ts
+++ src/describe/describe.ts
@@ -1,10 +1,10 @@
-import uvu from 'uvu';
+import { suite as uvuSuite } from 'uvu';
 import { UvuSuite } from '../types';
 
 export type DescribedContent = (suite: UvuSuite) => void;
 
 export function describe(name: string, described: DescribedContent) {
-  const suite = uvu.suite(name);
+  const suite = uvuSuite(name);
   described(suite);
   suite.run();
 }
